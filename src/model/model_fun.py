7dcedc00cbc2213ac9e860007ca4b9a0a201bd2f
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2022-03-10 15:33:24 +0600

    additional return of time added from createSample function

--- src/model/model_fun.py
+++ src/model/model_fun.py
@@ -1,18 +1,26 @@
-def createSample(sample):
-    """
-    returns a,b
-    where a is the code of the fabric
-      and b is the type of defect
-    """
-    import time
-    import random
-    import string
-    NN = ""
 
-    for i in range(sample):
-        NN = ''.join(random.choice(string.ascii_letters) for x in range(5))
-        for j in range(random.randint(10, 15)):
-            n = random.randint(0, 22)
-            yield NN, n
-            time.sleep(1)
-        time.sleep(15)
+
+def createSample(sample):
+  """
+  returns a,b
+  where a is the code of the fabric
+    and b is the type of defect
+  """
+  import time
+  import random
+  import string
+  from datetime import datetime
+
+ 
+
+  for i in range(sample):
+      random_String = ''.join(random.choice(string.ascii_letters) for x in range(5))
+      now = datetime.now()
+      for j in range(random.randint(10, 15)):
+          defectID = random.randint(1, 22)
+          yield random_String, defectID , now
+          time.sleep(1)
+      time.sleep(15)
+
+
+ 
